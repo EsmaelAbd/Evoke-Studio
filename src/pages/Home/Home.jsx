0a91a34a73c23706588b016d5bd6c83a9812335c
EsmaelAbd <esmaelabd51@gmail.com>
2024-11-23 18:10:58 +0300

    new updates

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 import HeroBG from "../../Gallery/ny6to4itoey-1.jpg";
 import rightThumbnai from "../../Gallery/evoke-resize/portrait/_MG_0534-1.jpg";
 import leftThumbnail from "../../Gallery/evoke-resize/portrait/IMG_3659-1.jpg";
 import WeddingBG from "../../Gallery/evoke-resize/wedding/IMG_6469-11-1.jpg";
-import { useNavigate } from "react-router-dom";
+import firstRow1 from "../../Gallery/evoke-resize/portrait/_MG_4915-1.jpg 1.jpg";
+import firstRow2 from "../../Gallery/evoke-resize/portrait/_MG_5624-1.jpg";
+import firstRow3 from "../../Gallery/evoke-resize/portrait/IMG_3830-1.jpg";
+import secondRow1 from "../../Gallery/evoke-resize/product/_MG_8997-1.jpg";
+import secondRow2 from "../../Gallery/evoke-resize/product/_MG_9894-1.jpg";
+import secondRow3 from "../../Gallery/evoke-resize/product/_MG_8941-11.jpg";
+import thirdRow1 from "../../Gallery/evoke-resize/wedding/_MG_9963-1.jpg";
+import thirdRow2 from "../../Gallery/evoke-resize/wedding/IMG_1606-1.jpg";
+import thirdRow3 from "../../Gallery/evoke-resize/wedding/IMG_8057-1.jpg";
 
 const Home = () => {
+  // ---------- the function to show albums from each side
   useEffect(() => {
     const albumsSection = document.getElementById("album");
 
@@ -36,6 +46,26 @@ const Home = () => {
       {/* Hero Section */}
       <div className="hero">
         <img src={HeroBG} alt="" />
+        <div className="hero-content">
+          <h1>Evoke Studio</h1>
+          <div className="hero-images">
+            <div className="first-row">
+              <img src={firstRow1} />
+              <img src={firstRow2} />
+              <img src={firstRow3} />
+            </div>
+            <div className="second-row">
+              <img src={secondRow1} />
+              <img src={secondRow2} />
+              <img src={secondRow3} />
+            </div>
+            <div className="third-row">
+              <img src={thirdRow1} />
+              <img src={thirdRow3} />
+              <img src={thirdRow2} />
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* Second Section - Portrait */}
@@ -82,11 +112,11 @@ const Home = () => {
       <div className="book-now">
         <div className="in-border">
           <h1>Book your own portrait photogprahy session</h1>
-          <p>feel free to reach us through our social-media platform</p>
-          <p>
-            for more info <button>Click Here</button>
-          </p>
         </div>
+        <p>feel free to reach us through our social-media platform</p>
+        <p>
+          for more info <button>Click Here</button>
+        </p>
       </div>
     </div>
   );
--- src/pages/Portrait/Portrait.jsx
+++ src/pages/Portrait/Portrait.jsx
@@ -1,8 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Portrait.css";
+import image1 from "../../Gallery/evoke-resize/portrait/_MG_0534-1.jpg";
+import image2 from "../../Gallery/evoke-resize/portrait/_MG_0548-1 bw.jpg";
+import image3 from "../../Gallery/evoke-resize/portrait/_MG_0548-1.jpg";
+import image4 from "../../Gallery/evoke-resize/portrait/_MG_0556-1 bw.jpg";
+import image5 from "../../Gallery/evoke-resize/portrait/_MG_0556-1.jpg";
+import image6 from "../../Gallery/evoke-resize/portrait/_MG_0567-1 bw.jpg";
+import image7 from "../../Gallery/evoke-resize/portrait/_MG_0567-1.jpg";
+import image8 from "../../Gallery/evoke-resize/portrait/_MG_0573-1 bw.jpg";
+import image9 from "../../Gallery/evoke-resize/portrait/_MG_0573-1.jpg";
+import image10 from "../../Gallery/evoke-resize/portrait/_MG_4915-1.jpg 1.jpg";
+import image11 from "../../Gallery/evoke-resize/portrait/_MG_4929-1.jpg";
+import image12 from "../../Gallery/evoke-resize/portrait/_MG_4930-1.jpg";
+import image13 from "../../Gallery/evoke-resize/portrait/_MG_4941-1.jpg 1.jpg";
+import image14 from "../../Gallery/evoke-resize/portrait/_MG_4943-1.jpg";
+import image15 from "../../Gallery/evoke-resize/portrait/_MG_4984-1.jpg";
+import image16 from "../../Gallery/evoke-resize/portrait/_MG_4986-1.jpg";
+import image17 from "../../Gallery/evoke-resize/portrait/_MG_5247-1.jpg";
+import image18 from "../../Gallery/evoke-resize/portrait/_MG_5252-1.jpg";
+import image19 from "../../Gallery/evoke-resize/portrait/_MG_5307-1.jpg";
+import image20 from "../../Gallery/evoke-resize/portrait/_MG_5314-1.psd 2.jpg";
+import image21 from "../../Gallery/evoke-resize/portrait/_MG_5316-1.jpg";
+import image22 from "../../Gallery/evoke-resize/portrait/_MG_5318-1.jpg";
+import image23 from "../../Gallery/evoke-resize/portrait/_MG_5327-1.jpg";
+import image24 from "../../Gallery/evoke-resize/portrait/_MG_5332-1.jpg 1.jpg";
+import image25 from "../../Gallery/evoke-resize/portrait/_MG_5333-1.jpg";
+import image26 from "../../Gallery/evoke-resize/portrait/_MG_5624-1.jpg";
+import image27 from "../../Gallery/evoke-resize/portrait/_MG_5659-1.jpg";
+import image28 from "../../Gallery/evoke-resize/portrait/_MG_8264-1.jpg";
+import image29 from "../../Gallery/evoke-resize/portrait/_MG_8268-1.jpg";
+import image30 from "../../Gallery/evoke-resize/portrait/_MG_8272-1.jpg";
+import image31 from "../../Gallery/evoke-resize/portrait/_MG_8274-1.jpg";
+import image32 from "../../Gallery/evoke-resize/portrait/IMG_3314-1.jpg";
+import image33 from "../../Gallery/evoke-resize/portrait/IMG_3339-1.jpg";
+import image34 from "../../Gallery/evoke-resize/portrait/IMG_3408-1.jpg";
+import image35 from "../../Gallery/evoke-resize/portrait/IMG_3618-1.jpg";
+import image36 from "../../Gallery/evoke-resize/portrait/IMG_3655-1.jpg";
+import image37 from "../../Gallery/evoke-resize/portrait/IMG_3659-1.jpg";
+import image38 from "../../Gallery/evoke-resize/portrait/IMG_3780-1.jpg";
+import image39 from "../../Gallery/evoke-resize/portrait/IMG_3804-1.jpg";
+import image40 from "../../Gallery/evoke-resize/portrait/IMG_3830-1.jpg";
 
 const Portrait = () => {
-  return <div>Portrait</div>;
+  return (
+    <div className="portrait">
+      <div className="portrait-section-images">
+        <div className="first-column">
+          <img src={image1} />
+          <img src={image2} />
+          <img src={image3} />
+          <img src={image4} />
+          <img src={image5} />
+          <img src={image6} />
+          <img src={image7} />
+          <img src={image8} />
+          <img src={image9} />
+          <img src={image10} />
+        </div>
+        <div className="second-column">
+          <img src={image11} />
+          <img src={image12} />
+          <img src={image13} />
+          <img src={image14} />
+          <img src={image15} />
+          <img src={image16} />
+          <img src={image17} />
+          <img src={image18} />
+          <img src={image19} />
+          <img src={image20} />
+        </div>
+        <div className="third-column">
+          <img src={image21} />
+          <img src={image22} />
+          <img src={image23} />
+          <img src={image24} />
+          <img src={image25} />
+          <img src={image26} />
+          <img src={image27} />
+          <img src={image28} />
+          <img src={image29} />
+          <img src={image30} />
+        </div>
+        <div className="fourth-column">
+          <img src={image31} />
+          <img src={image32} />
+          <img src={image33} />
+          <img src={image34} />
+          <img src={image35} />
+          <img src={image36} />
+          <img src={image37} />
+          <img src={image38} />
+          <img src={image39} />
+          <img src={image40} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Portrait;
